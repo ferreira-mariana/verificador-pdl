946d61e04478d58d887e138c85670765c872e83f
Mariana Ferreira <ferreira_mariana@id.uff.br>
2019-06-16 22:22:06 -0300

    casos da Logica Proposicional

--- pdl.hs
+++ pdl.hs
@@ -2,16 +2,39 @@
 data Arv = Null | Fo String | No String Arv Arv deriving (Show, Read, Eq)
 
 isSatisfied :: Arv -> String
-isSatisfied x = "ok"
+isSatisfied x
+    | isSatisfiedBool(x) == False = "Nao vale"
+    | isSatisfiedBool(x) == True = "Vale"
+--tratar os casos em que precisamos mostrar o caminho no modelo
+
+
+isSatisfiedBool :: Arv -> Bool
+isSatisfiedBool (Fo x) = False 
+isSatisfiedBool (No x (right) (left))
+    | x == "~" =  not(isSatisfiedBool(right))
+    | x == "^" = isSatisfiedBool(right) && isSatisfiedBool(left)
+    | x == "v" = isSatisfiedBool(right) || isSatisfiedBool(left)
+    | x == "->" = isSatisfiedBool(right) `implica` isSatisfiedBool(left)
+    | x == "<->" = isSatisfiedBool(right) `biImplica` isSatisfiedBool(left)
+isSatisfiedBool x = error "caso nao tratado" 
+
+
+modelo = [["a", "b", "alpha"], ["a", "c", "beta"], ["c", "d", "alpha"]]
+-- chamar recursivamente pros dois lados no modelo
+
 
 --implicacao (->)
-implicacao :: Bool -> Bool -> Bool 
-implicacao p q
+implica :: Bool -> Bool -> Bool 
+implica p q
     | (p == True && q == False) = False 
     | otherwise = True 
 
 --bi-implicacao (<->)
-biImplicacao :: Bool -> Bool -> Bool
-biImplicacao p q
+biImplica :: Bool -> Bool -> Bool
+biImplica p q
     | (p == q) = True
     | otherwise = False  
+
+--obs:
+-- (No "~" (Fo "p") (Null) )
+-- quando tivermos negaçao, o filho a direita será sempre vazio
